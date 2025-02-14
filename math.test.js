1661a7eabc32d2669db7bd9aba0125450207b7f9
Luong Quang Hung <111416811+kudo-2003@users.noreply.github.com>
2025-02-14 18:18:08 +0000

    Mô tả thay đổi

---
+++ math.test.js
@@ -0,0 +1,9 @@
+const add = require('./math');
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test('adds -2 + -3 to equal -5', () => {
+  expect(add(-2, -3)).toBe(-5);
+});
